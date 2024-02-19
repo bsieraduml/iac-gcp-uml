099073c43394182ad3582ccfb8d292f9864fd615
Benjamin Sieradzki <Benjamin_Sieradzki@student.uml.edu>
2024-02-19 12:29:32 -0500

    test subnet

--- main.tf
+++ main.tf
@@ -2,11 +2,18 @@
 
 provider "google" {
   //credentials = file("creds.json")
-  project     = "project-gcp-uml"
-  region      = var.region
-  zone        = "us-east1-a"
+  project = "project-gcp-uml"
+  region  = var.region
+  zone    = "us-east1-a"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "vnet-gcp-uml"
+}
+
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "subnet-private-01-gcp-uml"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
 }
