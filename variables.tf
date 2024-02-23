be35dcf296147648396dcbd5255a916ec543e122
Benjamin Sieradzki <Benjamin_Sieradzki@student.uml.edu>
2024-02-23 11:07:11 -0500

    test

--- variables.tf
+++ variables.tf
@@ -3,11 +3,14 @@
 # //ben todo
 # //this may go away when I go to Terraform Cloud; ditto with creds.json
 # # variable "credentials_file" {}
+variable "project_id" {
+  default = "project-gcp-uml"
+}
 
 variable "region" {
   default = "us-east1"
 }
 
-# variable "zone" {
-#   default = "us-east1-a"
-# }
+variable "zone" {
+  default = "us-east1-a"
+}
