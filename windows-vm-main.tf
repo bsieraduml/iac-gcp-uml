2aa91cd8ab4852faa9a5dcba1d824fd584fee62d
Benjamin Sieradzki <Benjamin_Sieradzki@student.uml.edu>
2024-02-24 10:47:45 -0500

    fix lb 6;

--- windows-vm-main.tf
+++ windows-vm-main.tf
@@ -27,9 +27,11 @@ EOF
 }
 
 # Create VM
+#hostname = "${lower(each.key)}-${var.app_name}-vm${random_id.instance_id.hex}.${var.app_domain}"
+#name = "${lower(each.key)}-${lower(var.company)}-${lower(var.app_name)}-${var.environment}-vm${random_id.instance_id.hex}"
 resource "google_compute_instance" "vm_instance_public" {
 for_each = var.myvms
-  name         = "${lower(each.key)}-${lower(var.company)}-${lower(var.app_name)}-${var.environment}-vm${random_id.instance_id.hex}"
+  name         = "${lower(each.key)}-${lower(var.company)}-${lower(var.app_name)}-${var.environment}"
   machine_type = var.windows_instance_type
   zone         = each.value.zone
   hostname     = "${lower(each.key)}-${var.app_name}-vm${random_id.instance_id.hex}.${var.app_domain}"
@@ -77,9 +79,8 @@ resource "google_compute_forwarding_rule" "frontend_lb" {
 #health_checks = ["https://www.googleapis.com/compute/beta/projects/project-gcp-uml/global/httpHealthChecks/health-check-lb"]
 #instances = ["us-east1-b/vm1-bsierad-iac-windows-uml-dev-vm239dea64", "us-east1-c/vm2-bsierad-iac-windows-uml-dev-vm239dea64", "us-east1-d/vm3-bsierad-iac-windows-uml-dev-vm239dea64"]
 resource "google_compute_target_pool" "lb_test" {
-  for_each = var.myvms
   health_checks    = [google_compute_http_health_check.health_check_lb.name]
-  instances        = ["${each.value.zone}/${lower(each.key)}-${var.app_name}-vm${random_id.instance_id.hex}.${var.app_domain}"]
+  instances        = ["us-east1-b/vm1-bsierad-iac-windows-uml-dev", "us-east1-c/vm2-bsierad-iac-windows-uml-dev", "us-east1-d/vm3-bsierad-iac-windows-uml-dev"]
   name             = "lb-test"
   project          = "project-gcp-uml"
   region           = "us-east1"
