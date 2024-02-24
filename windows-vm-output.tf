034400f6eacbf35e2fbb88b8b9861391a02e778a
Benjamin Sieradzki <Benjamin_Sieradzki@student.uml.edu>
2024-02-23 20:23:56 -0500

    fix output;

--- windows-vm-output.tf
+++ windows-vm-output.tf
@@ -6,6 +6,14 @@ output "vm-name" {
   value = { for vm in keys(var.myvms) : vm => google_compute_instance.vm_instance_public[vm].name}
 }
 
+output "vm-external-ip" {
+  value = { for vm in keys(var.myvms) : vm => google_compute_instance.vm_instance_public[vm].network_interface.0.access_config.0.nat_ip}
+}
+
+output "vm-internal-ip" {
+  value = { for vm in keys(var.myvms) : vm => google_compute_instance.vm_instance_public[vm].network_interface.0.network_ip}
+}
+
 # output "vm-name" {
 #   value = google_compute_instance.vm_instance_public.*.name
 # }
