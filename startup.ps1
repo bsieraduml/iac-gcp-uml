fdafa00e0badbcd972c5f6eee33d3555cd9514ec
Benjamin Sieradzki <Benjamin_Sieradzki@student.uml.edu>
2024-03-03 12:30:41 -0500

    fix ps;

--- startup.ps1
+++ startup.ps1
@@ -8,6 +8,21 @@ Remove-Item -Path "C:\inetpub\wwwroot\*" -Force -Recurse
 $metadataUrl = "http://metadata.google.internal/computeMetadata/v1/instance/name"
 $instanceName = Invoke-RestMethod -Uri $metadataUrl -Headers @{"Metadata-Flavor"="Google"}
 
+# Create a new index.html file with the instance name
+# @"
+# <!DOCTYPE html>
+# <html>
+# <head>
+#     <title>Welcome to $instanceName</title>
+# </head>
+# <body>
+#     <h1>Welcome to $instanceName</h1>
+#     <p>This page is served by an IIS instance running on a Windows Server VM in Google Cloud Platform.</p>
+# </body>
+# </html>
+# "@ | Set-Content -Path "C:\inetpub\wwwroot\index.html"
+
+
 # Set the GCP project ID and GCS bucket name
 $projectId = "bentest-415415"
 $bucketName = "bsierad-uml-iac-windows-website"
@@ -23,3 +38,17 @@ $localFolderPath = "C:\inetpub\wwwroot"
 $gsutilListCommand = "gsutil -m rsync -r -x '^(?!.*\.*$).*' gs://$bucketName c:\inetpub\wwwroot"
 $gsutilListOutput = Invoke-Expression -Command $gsutilListCommand
 
+# # Create the local folder if it doesn't exist
+# if (-not (Test-Path -Path $localFolderPath -PathType Container)) {
+#     New-Item -Path $localFolderPath -ItemType Directory
+# }
+
+# # Use gsutil to list all files in the bucket and download them to the local folder
+# $gsutilListCommand = "gsutil ls gs://$bucketName"
+# $gsutilListOutput = Invoke-Expression -Command $gsutilListCommand
+
+# foreach ($fileUri in $gsutilListOutput) {
+#     $fileName = $fileUri.Substring($fileUri.LastIndexOf("/") + 1)
+#     $localFilePath = Join-Path -Path $localFolderPath -ChildPath $fileName
+#     gsutil cp $fileUri $localFilePath
+# }
